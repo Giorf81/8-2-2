8bdf43d2db257d58db4df43381221f6afbbc949d
Giorf81 <rejroberts7@gmail.com>
2025-01-15 22:07:53 +0300

    Добавьте файлы проекта.

---
+++ 8-2-2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _8_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"C:\\");
+                if (dirInfo.Exists)
+                {
+                    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                }
+                DirectoryInfo directoryInfo = new DirectoryInfo (@"C:\Новая ПапкаС");
+                if (!directoryInfo.Exists)
+                    directoryInfo.Create();
+                Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+
+            }
+            catch {
+            Console.WriteLine("чето не совсем ваушное");
+            }
+
+
+
+
+        }
+    }
+}
